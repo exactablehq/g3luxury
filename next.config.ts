658627cc40e54def89ca0bb51b1a22d2fd5e6ade
Jaynam Modi <jaynammodi@gmail.com>
2026-08-07 12:45:04 +0530

    fixed images

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
+  // images: {
+  //   minimumCacheTTL: 31536000, // 1 year cache TTL for optimized images
+  //   formats: ["image/avif", "image/webp"],
+  // },
   images: {
-    minimumCacheTTL: 31536000, // 1 year cache TTL for optimized images
-    formats: ["image/avif", "image/webp"],
+    unoptimized: true,
   },
   // allowedDevOrigins: ["192.168.1.8"],
   output: "export",
